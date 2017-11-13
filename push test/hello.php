182019341c927e8b279a6fb33c557f5b046613f7
shohei-uto <shohei1997828@gmail.com>
2017-11-13 22:58:15 +0900

    test commit

---
+++ push test/hello.php
@@ -0,0 +1,16 @@
+<html>
+<!--<head>
+<title>PHPの基本</title>
+</head>-->
+<body>
+<?php
+// print命令は指定された文字列を表示するための命令です。
+print"こんにちは、世界！<br/>";
+print"こんにちは、皆さん！";
+$msg = "こんにちは、世界！";
+
+print $msg; //結果：こんにちは、世界
+
+?>
+</body>
+</html>
